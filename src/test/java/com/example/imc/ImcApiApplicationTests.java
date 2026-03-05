4263952113f8d5c95dde3e5a7cd559addac4c217
VINICIUS NASCIMENTO DE MELO <vinicius.melo2@alunos.sc.senac.br>
2026-03-05 20:18:18 -0300

    primeiro commit

---
+++ src/test/java/com/example/imc/ImcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
